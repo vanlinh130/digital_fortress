bb61b93e25a252fcedbf2bf24a552b88ad9db266
Văn Linh Lê <linhvanle130@gmail.com>
2024-09-06 14:49:06 +0700

    login

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -1,64 +1,72 @@
 import Image from 'next/image';
 import login from '../../public/Illustration.png';
+import { TfiEmail } from 'react-icons/tfi';
+import { PiLockKey } from 'react-icons/pi';
 
 export default function Login() {
     return (
-        <div className="flex h-screen">
-            <div className="w-1/2 bg-[#0D0F10] text-white flex flex-col justify-center px-12">
-                <h1 className="text-4xl font-semibold mb-6">
-                    Hello, <span className="">Digital Fortress</span>
-                </h1>
-                <p className="mb-4 text-[#6F767E]">Log in to platform to start creating magic.</p>
+        <div className="flex bg-[#131619]  h-screen">
+            <div className=" w-1/2 bg-[#0D0F10] flex flex-col justify-center items-center">
+                <div className="flex flex-col gap-16 w-[496px] text-white px-12">
+                    <div className="flex flex-col gap-6">
+                        <h1 className="text-4xl font-extralight">
+                            Hello, <span className="gradient-heading font-semibold">Digital Fortress</span>
+                        </h1>
+                        <p className="text-[16px] mb-4 text-[#6F767E]">Log in to platform to start creating magic.</p>
+                    </div>
 
-                <form className="space-y-4">
-                    <div>
-                        <label className="block text-sm font-medium">Email</label>
-                        <input
-                            type="email"
-                            className="w-full p-3 mt-1 bg-gray-800 rounded-md focus:ring-2 focus:ring-green-400 focus:outline-none"
-                            placeholder="candidate.digitalfortress@gmail.com"
-                        />
-                    </div>
-                    <div>
-                        <label className="block text-sm font-medium">Password</label>
-                        <input
-                            type="password"
-                            className="w-full p-3 mt-1 bg-gray-800 rounded-md focus:ring-2 focus:ring-green-400 focus:outline-none"
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <label className="flex items-center">
-                            <input type="checkbox" className="form-checkbox bg-gray-800 rounded-md" />
-                            <span className="ml-2 text-sm text-[#6F767E]">Remember me</span>
-                        </label>
-                        <a href="#" className="text-sm text-green-400 ">
-                            Forgot Password?
-                        </a>
-                    </div>
-                    <button className="w-full bg-green-500 hover:bg-green-600 p-3 rounded-md text-white font-semibold">
-                        Log in
-                    </button>
-                </form>
-
-                <p className="mt-6 text-sm text-center text-[#6F767E]">
-                    Don’t have an account?{' '}
-                    <a href="/dashboard" className="text-green-400">
-                        Sign Up
-                    </a>
-                </p>
+                    <form className="flex flex-col gap-12">
+                        <div className="flex flex-col gap-4">
+                            <div className="relative mb-6 ">
+                                <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                                    <TfiEmail className="text-[24px] text-gray-500 " />
+                                </div>
+                                <input
+                                    type="text"
+                                    id=""
+                                    className="bg-[#1A1D21] text-sm rounded-xl block w-full ps-12 px-4 py-3 focus:ring-2 focus:ring-[#B6F09C] focus:outline-none caret-[#B6F09C]"
+                                    placeholder="Email"
+                                />
+                            </div>
+                            <div className="relative mb-6 ">
+                                <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                                    <PiLockKey className="text-[24px] text-gray-500 " />
+                                </div>
+                                <input
+                                    type="password"
+                                    id=""
+                                    className="bg-[#1A1D21] text-sm rounded-xl block w-full ps-12 px-4 py-3 focus:ring-2 focus:ring-[#B6F09C] focus:outline-none caret-[#B6F09C]"
+                                    placeholder="Password"
+                                />
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <label className="flex gap-4 items-center">
+                                <input
+                                    type="checkbox"
+                                    className="bg-[#1A1D21] w-[24px] h-[24px] rounded-[4px] border-[1px] border-[#363A3D]"
+                                />
+                                <span className="text-sm text-[#CDCECF] font-medium">Remember me</span>
+                            </label>
+                            <a href="#" className="text-sm gradient-text ">
+                                Forgot Password?
+                            </a>
+                        </div>
+                        <button className="w-full bg-[#B6F09C] hover:bg-[#9ff876] py-2 px-6 rounded-xl text-[#0C1132] text-[16px] font-semibold">
+                            Log in
+                        </button>
+                        <p className="mt-6 text-sm text-center text-[#6F767E]">
+                            Don’t have an account?{' '}
+                            <a href="/dashboard" className="gradient-text">
+                                Sign Up
+                            </a>
+                        </p>
+                    </form>
+                </div>
             </div>
 
-            <div className="w-1/2 relative">
-                <div className="w-full h-full">
-                    <Image
-                        src={login}
-                        alt="Background"
-                        layout="fill"
-                        objectFit="cover"
-                        className="rounded-r-lg w-[720px] h-[1024px]"
-                    />
-                </div>
+            <div className="w-1/2">
+                <Image src={login} alt="Background" className="object-fill w-full h-full" />
             </div>
         </div>
     );
